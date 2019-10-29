6258e4b14e66cdbf3c86483472321a737e747127
JarlTuxen <jarl.tuxen@gmail.com>
2019-10-29 11:53:34 +0100

    hello world

---
+++ src/main/java/dk/kea/dat18c/dockerdemo/DockerdemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.dat18c.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerdemoApplication.class, args);
+    }
+
+}
