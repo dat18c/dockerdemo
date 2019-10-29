26917871fecdde5e5d89e56e4a99182a944d02fc
JarlTuxen <jarl.tuxen@gmail.com>
2019-10-29 12:22:56 +0100

    controller

---
+++ src/main/java/dk/kea/dat18c/dockerdemo/Controller/MainController.java
@@ -0,0 +1,14 @@
+package dk.kea.dat18c.dockerdemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
